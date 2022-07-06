fb66d1668facdd6bbcc53e44de4bcdd9a0fa36ee
Ben Shelton <bks-44@hotmail.com>
2022-07-06 16:04:57 -0500

    chapter 8

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,25 @@
+import { fetchRequests, fetchPlumbers } from "./dataAccess.js"
 import { SinkRepair } from "./SinkRepair.js"
 
 
 const mainContainer = document.querySelector("#container")
 
 const render = () => {
-    mainContainer.innerHTML = SinkRepair()
+    fetchRequests()
+        .then(() => fetchPlumbers())
+        .then(
+            () => {
+                mainContainer.innerHTML = SinkRepair()
+            }
+        )
 }
 
 render()
+
+document.addEventListener(
+    "stateChanged",
+    customEvent => {
+        render()
+    }
+)
 
